Fix nms boxes with in half precision and unit test with new stateless plotting

src/flat_bug/nms.py
@@ -736,7 +736,8 @@ def nms_boxes(
     Wrapper for `torchvision.ops.nms`; the standard non-maximum suppression algorithm.
     """
     if overlap_fn is None or isinstance(overlap_fn, str) and (overlap_fn := overlap_fn.strip().lower()) == "iou":
-        return torchvision.ops.nms(boxes, scores, overlap_threshold).sort().values
+        with torch.autocast(device_type=boxes.device.type, dtype=boxes.dtype):
+            return torchvision.ops.nms(boxes, scores, overlap_threshold).sort().values
     if isinstance(overlap_fn, str):
         overlap_fn = get_overlap_fn("box", overlap_fn)
     return base_nms_(boxes, overlap_fn=overlap_fn, scores=scores, overlap_threshold=overlap_fn, return_indices=True)